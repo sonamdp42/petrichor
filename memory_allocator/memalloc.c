1e5ef88d307e8ee41fb62325d7e12b9e8f1eb588
sonam <sonam.dp42@gmail.com>
2013-11-19 12:33:21 -0500

    Added bug fixes to memory allocator code.

--- memory_allocator/memalloc.c
+++ memory_allocator/memalloc.c
@@ -11,8 +11,8 @@ struct free_list {
 	struct free_list *prev;
 };
 
-void * memalloc_head = NULL;
-void * memalloc_start = NULL;
+char * memalloc_head = NULL;
+char * memalloc_start = NULL;
 
 /* Always allocate memory in multiples of min block size */
 size_t closest_multiple_of_min_block(size_t size)
@@ -25,21 +25,15 @@ void * memalloc(size_t size)
 	uint32_t magic = MAGIC;
 	size_t to_allocate, aligned_size, cur_size, free_size;
 	struct free_list flist, *freeList = NULL, *itr = NULL;
-	void *prev = NULL, *next = NULL, *ret = NULL;
+	void *prev = NULL, *next = NULL, *ret = NULL, *tmp = NULL;
 	size_t min_mem = sizeof(size_t) + sizeof(uint32_t) + sizeof(uint32_t);
 
-	//printf("Enter: %s\n", __FUNCTION__);
-	printf("Enter: %s, magic: %u, MAGIC: %u\n", __FUNCTION__, magic, MAGIC);
 	assert(size > 0);
 
 	aligned_size = closest_multiple_of_min_block(size);
 
-	printf("aligned size: %lu, size: %lu\n", aligned_size, size);
-
 	/* First check if memalloc_head is NULL */
 	if (!memalloc_head || !memalloc_start) {
-		printf("First element added, calling sbrk()\n");
-
 		if (aligned_size >= INCREMENT_LEN)
 			to_allocate = aligned_size + INCREMENT_LEN;
 		else
@@ -51,13 +45,11 @@ void * memalloc(size_t size)
 			return NULL;
 		}
 
+		memset(memalloc_start, 0, to_allocate);
+
 		memcpy(memalloc_start, &aligned_size, sizeof(size_t));
 		memcpy(memalloc_start + sizeof(size_t), &magic, sizeof(uint32_t));
 		memcpy(memalloc_start + sizeof(size_t) + sizeof(uint32_t) + aligned_size, &magic, sizeof(uint32_t));
-
-		size_t verifylen = 0;
-		memcpy(&verifylen, memalloc_start, sizeof(size_t));
-		printf("verified lem: %lu\n", verifylen);
 
 		flist.size = INCREMENT_LEN - aligned_size - min_mem;
 		flist.next = NULL;
@@ -73,48 +65,41 @@ void * memalloc(size_t size)
 	itr = freeList;
 
 	while(itr) {
-		printf("Iterating....\n");
-
 		if (itr->size - min_mem >= aligned_size) {
 			free_size = itr->size;
 			prev = itr->prev;
 			next = itr->next;
 
-			printf("free size: %lu\n", free_size);
-
-			ret = itr;
+			ret = (char *) itr;
 
 			if (aligned_size < itr->size - 2 * min_mem - MIN_BLOCK_SIZE) {
-				printf("We have enough space for a free slot here.\n");
 				/* We have enough space for a free slot here. */
-				itr = itr + aligned_size + min_mem;
+				tmp = (char *) itr + aligned_size + min_mem;
 
 				/* Update previous free slot's next pointer */
 				if (prev) {
-					printf("Prev exists\n");
 					memcpy(&flist, prev, sizeof(struct free_list));
-					flist.next = itr;
+					flist.next = tmp;
 					memcpy(prev, &flist, sizeof(struct free_list));
 				}
 
 				/* Update next free slot's prev pointer */
 				if (next) {
-					printf("Next exists\n");
 					memcpy(&flist, next, sizeof(struct free_list));
-					flist.prev = itr;
+					flist.prev = tmp;
 					memcpy(next, &flist, sizeof(struct free_list));
 				}
 
 				/* Add entry for this free slot */
-				printf("Memcpy not done!\n");
 				flist.size = free_size - aligned_size - min_mem;
 				flist.prev = prev;
 				flist.next = next;
-				printf("Memcpy not done 2! %u\n", flist.size);
-				memcpy(itr, &flist, sizeof(struct free_list));
-				printf("Memcpy done!\n");
+				memset(tmp, 0, sizeof(struct free_list));
+				memcpy(tmp, &flist, sizeof(struct free_list));
+
+				if (ret == memalloc_head)
+					memalloc_head = tmp;
 			} else {
-				printf("We do not have enough space for a free slot here.\n");
 				/* No space for free slot. */
 				aligned_size = free_size;
 
@@ -133,7 +118,7 @@ void * memalloc(size_t size)
 				}
 
 				/* Set memalloc_head to next/prev free slot */
-				if (itr == memalloc_head)
+				if ((char *)itr == memalloc_head)
 					memalloc_head = next;
 			}
 
@@ -162,6 +147,8 @@ void * memalloc(size_t size)
 		return NULL;
 	}
 
+	memset(ret, 0, to_allocate);
+
 	prev = itr->prev;
 	next = itr->next;
 	free_size = itr->size;
@@ -182,7 +169,6 @@ void * memalloc(size_t size)
 
 	memcpy(itr, &flist, sizeof(struct free_list));
 
-	printf("Exit: %s\n", __FUNCTION__);
 	return ret + min_mem - sizeof(uint32_t);
 }
 
@@ -192,14 +178,10 @@ void memfree(void * data)
 	uint32_t magic = MAGIC;
 	struct free_list *freeList = NULL, *itr = NULL, *prev = NULL, *next = NULL, flist;
 
-	printf("Enter: %s, magic: %u, MAGIC: %u\n", __FUNCTION__, magic, MAGIC);
-
 	assert(data);
 
 	memcpy(&length, data - sizeof(size_t) - sizeof(uint32_t), sizeof(size_t));
 	assert(length > 0);
-
-	printf("length obtained: %lu\n", length);
 
 	/* Check if Magic is in place -> detect double free/memory corruption */
 	if (memcmp(data - sizeof(uint32_t), &magic, sizeof(uint32_t))) {
@@ -215,7 +197,6 @@ void memfree(void * data)
 	/* Need to identify prev and next on free list to this slot */
 	freeList = (struct free_list *) memalloc_head;
 	if (!freeList->next) {
-		printf("Only one entry in free list\n");
 		prev = freeList;
 		next = NULL;
 	} else {
@@ -234,13 +215,13 @@ void memfree(void * data)
 
 	if (prev) {
 		memcpy(&flist, prev, sizeof(struct free_list));
-		flist.next = data;
+		flist.next = data - sizeof(size_t) - sizeof(uint32_t);
 		memcpy(prev, &flist, sizeof(struct free_list));
 	}
 
 	if (next) {
 		memcpy(&flist, next, sizeof(struct free_list));
-		flist.prev = data;
+		flist.prev = data - sizeof(size_t) - sizeof(uint32_t);
 		memcpy(next, &flist, sizeof(struct free_list));
 	}
 
@@ -249,15 +230,12 @@ void memfree(void * data)
 	flist.prev = prev;
 	memcpy(data - sizeof(size_t) - sizeof(uint32_t), &flist, sizeof(struct free_list));
 
-	printf("Exit: %s\n", __FUNCTION__);
 	return;
 }
 
 int main(int argc, char *argv[])
 {
 	char *ptr1 = NULL, *ptr2 = NULL;
-
-	printf("sizeof char: %lu, sizeof int: %lu, sizeof size_t: %lu\n", sizeof(char), sizeof(int), sizeof(size_t));
 
 	ptr1 = (char *) memalloc(sizeof(char) * 150);
 	if (!ptr1) {
@@ -274,7 +252,7 @@ int main(int argc, char *argv[])
 		return;
 	}
 
-	sprintf(ptr2, "In dire need of a haircut, oh no, oh no, oh no, blah blah blah blah blah, blue blue blue blue blue blue blue\0");
+	sprintf(ptr2, "This is the way the world ends, not with a bang but a wimper. A quote by T.S. Eliot. Dun dun dun.................\0");
 	printf("String ptr2: %s\n", ptr2);
 
 	memfree(ptr1);
